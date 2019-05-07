628f264f62794c00366f6d226346bf409ea71ee8
Mila-Zagrevskaya <mila.zagrevskaya@gmail.com>
2019-05-07 14:45:41 +0300

    slider

--- js/database.js
+++ js/database.js
@@ -1,3 +1,16 @@
-kharkov {
+const kharkov = [
+	{
+		name: 'ЦПКтаВ ім. М. Горького',
+		adress: "вул. Сумська, 81",
+		coordinates: {lat: 50.0163106, lng: 36.246678},
+		image: "",
+		info: ""
 
-}
+
+	},
+]
+
+
+
+
+// Key google maps API - AIzaSyDywW1xNiLBD-ySMGdGX-3Y9ZWj3OvxhR0
