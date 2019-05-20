fad49e5d43500b3943dd8acfd977dbde4c01866e
Mila-Zagrevskaya <mila.zagrevskaya@gmail.com>
2019-05-20 17:14:17 +0300

    change footer

--- public/js/myMap.js
+++ public/js/myMap.js
@@ -1,25 +1,22 @@
-// let myMap;
+// // let myMap;
+// let markerArray = []
 
 
-// let makeWay =  
-// fetch ("../js/maketheway.js")
-//     .then (response => response.text ()
-//       .then (
-//         response => { 
-//           document.body.appendChild(
-//             document.createElement('script')).textContent = response 
-//         }
-//       )
-//     )
-
-
-
-
-
-
+// // let makeWay =  
+// // fetch ("../js/maketheway.js")
+// //     .then (response => response.text ()
+// //       .then (
+// //         response => { 
+// //           document.body.appendChild(
+// //             document.createElement('script')).textContent = response 
+// //         }
+// //       )
+// //     )
 
 // function initMap() {
-//   console.log (this.cityArr)
+//   // console.log (this.cityArr)
+//   // let directionsService
+//   // let directionsDisplay
 //   let container =  document.getElementById('mymap')
 //   let options = {
 //       mapTypeId: 'hybrid',
@@ -27,10 +24,7 @@
 //       zoom: 12
 //   }
 //   myMap = new google.maps.Map(container, options);
-  
-  
-  
-  
+    
 //   function addMarker (properties) {
 //     let marker = new google.maps.Marker ({
 //       map: myMap,
@@ -52,27 +46,70 @@
 //   })  
 
 
-//   var flightPlanCoordinates = markers.map (function (mark) {
-//      return mark.coordinates
-//   })
+//     // прокладывает путь от А до Б
+//   var directionsService = new google.maps.DirectionsService;
+//   var directionsDisplay = new google.maps.DirectionsRenderer({map: myMap});
+//     // Instantiate an info window to hold step text.
+//   var stepDisplay = new google.maps.InfoWindow;
+//     // Display the route between the initial start and end selections.
+//   calculateAndDisplayRoute( directionsDisplay, directionsService, markerArray, stepDisplay, myMap);
+//     // Listen to change events from the start and end lists.
+//   var onChangeHandler = function() {
+//     calculateAndDisplayRoute(
+//       directionsDisplay, directionsService, markerArray, stepDisplay, myMap);
+//   };
+//   document.getElementById('start').addEventListener('change', onChangeHandler);
+//   document.getElementById('end').addEventListener('change', onChangeHandler);
+//           }
 
+//   function calculateAndDisplayRoute(directionsDisplay, directionsService,
+//     markerArray, stepDisplay, myMap) {
+//     // First, remove any existing markers from the map.
+//     for (var i = 0; i < markerArray.length; i++) {
+//       markerArray[i].setMap(null);
+//     }
+//     // Retrieve the start and end locations and create a DirectionsRequest using
+//         // WALKING directions.
+//         directionsService.route({
+//           origin: document.getElementById('start').value,
+//           destination: document.getElementById('end').value,
+//           travelMode: 'WALKING'
+//         }, function(response, status) {
+//           // Route the directions and pass the response to a function to create
+//           // markers for each step.
+//           if (status === 'OK') {
+//             document.getElementById('warnings-panel').innerHTML =
+//                 '<b>' + response.routes[0].warnings + '</b>';
+//             directionsDisplay.setDirections(response);
+//             showSteps(response, markerArray, stepDisplay, map);
+//           } else {
+//             window.alert('Directions request failed due to ' + status);
+//           }
+//         });
+//   }
+//        function showSteps(directionResult, markerArray, stepDisplay, map) {
+//         // For each step, place a marker, and add the text to the marker's infowindow.
+//         // Also attach the marker to an array so we can keep track of it and remove it
+//         // when calculating new routes.
+//         var myRoute = directionResult.routes[0].legs[0];
+//         for (var i = 0; i < myRoute.steps.length; i++) {
+//           var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+//           marker.setMap(map);
+//           marker.setPosition(myRoute.steps[i].start_location);
+//           attachInstructionText(
+//               stepDisplay, marker, myRoute.steps[i].instructions, map);
+//         }
+//       }
 
-//   // var flightPlanCoordinates = function (){
-    
-//   // }
+//       function attachInstructionText(stepDisplay, marker, text, map) {
+//         google.maps.event.addListener(marker, 'click', function() {
+//           // Open an info window when the marker is clicked on, containing the text
+//           // of the step.
+//           stepDisplay.setContent(text);
+//           stepDisplay.open(map, marker);
+       
 
-//   // var flightPath = new google.maps.Polyline({
-//   //           path: flightPlanCoordinates,
-//   //           geodesic: true,
-//   //           strokeColor: '#FF0000',
-//   //           strokeOpacity: 1.0,
-//   //           strokeWeight: 2
-//   //         });
-
-//   // flightPath.setMap(myMap);
-
-
-
+//         })
 
 // }
 
